f19bec428ac65eaaad8feea11e5798ee9e69f918
blackprince001 <appiahboaduprince@gmail.com>
2023-03-05 14:22:29 +0000

    project init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "PlacementSystem/placement_system.cpp"
+#include "Schools/school.cpp"
+#include "Student/student.cpp"
+
+int main() {
+  std::cout << "Hello Project" << std::endl;
+
+  return 0;
+}
