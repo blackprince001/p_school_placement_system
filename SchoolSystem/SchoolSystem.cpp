d4d2c21ace7507d0ccab72728e0eb080414c8744
blackprince001 <appiahboaduprince@gmail.com>
2023-03-13 11:42:33 +0000

    implement evaluated grade computation member fnc

--- SchoolSystem/SchoolSystem.cpp
+++ SchoolSystem/SchoolSystem.cpp
@@ -10,12 +10,24 @@ std::string RegistedSchool::get_name() { return name; }
 
 std::string Student::get_name() { return name; }
 
-std::unordered_map<std::string, std::string> Student::get_grades() {
+std::unordered_map<std::string, std::string> Student::get_grades() const {
   return grades;
 }
 
-std::vector<RegistedSchool> Student::get_school_choices() {
+std::vector<RegistedSchool> Student::get_school_choices() const {
   return school_choices;
+}
+
+int Student::get_computed_evaluation() {
+  for (auto &[_, grade] : grades) {
+    if (grade == "A") evaluated_grade += 1;
+    if (grade == "B") evaluated_grade += 2;
+    if (grade == "C") evaluated_grade += 3;
+    if (grade == "D") evaluated_grade += 4;
+    if (grade == "E") evaluated_grade += 5;
+    if (grade == "F") evaluated_grade += 6;
+  }
+  return evaluated_grade;
 }
 
 void Student::display_profile() {
