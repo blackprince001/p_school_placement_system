b8d55ac1c50a661d22fe8cfec5ae9bcf9331d771
blackprince001 <appiahboaduprince@gmail.com>
2023-03-13 11:42:55 +0000

    add placement system attributes and member functions

--- PlacementSystem/placement_system.hpp
+++ PlacementSystem/placement_system.hpp
@@ -1,11 +1,29 @@
+#pragma once
 
 #include <string>
+#include <tuple>
 #include <vector>
 
-namespace System {
+#include "../SchoolSystem/SchoolSystem.hpp"
 
 class PlacementSystem {
-  std::vector<std::string> schools;
-};
+ private:
+  std::vector<RegistedSchool> db_schools;
+  std::vector<Student> db_students;
+  std::vector<std::pair<Student, RegistedSchool>> placed_students;
+  // TODO add attributes for student analytics and review
 
-}  // namespace System
+ public:
+  PlacementSystem() : db_schools({}), db_students({}){};
+  PlacementSystem(std::vector<RegistedSchool> &schoolList,
+                  std::vector<Student> &studentList)
+      : db_schools(schoolList), db_students(studentList){};
+  std::vector<Student> get_students();
+  void add_school(RegistedSchool &);
+  void add_student(Student &);
+  std::vector<RegistedSchool> get_registed_schools();
+  void place_students();
+  std::vector<std::pair<Student, RegistedSchool>> get_placed_students() {
+    return placed_students;
+  }
+};
