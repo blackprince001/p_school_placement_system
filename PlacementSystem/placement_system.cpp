f38183abd33a777dec513f4470cd56d6e311d34b
blackprince001 <appiahboaduprince@gmail.com>
2023-03-14 03:07:12 +0000

    implement placement and other member functions

--- PlacementSystem/placement_system.cpp
+++ PlacementSystem/placement_system.cpp
@@ -22,18 +22,39 @@ std::vector<RegistedSchool> PlacementSystem::get_registed_schools() {
   return db_schools;
 }
 
+std::vector<std::pair<Student, RegistedSchool>>
+PlacementSystem::get_placed_students() {
+  if (db_students.size() > 1 && placed_students.size() <= 1)
+    return placed_students;
+  return {};  // else return an empty vector
+}
+
+std::vector<std::pair<Student, RegistedSchool>>
+PlacementSystem::get_rejected_students() {
+  if (db_students.size() > 1 && rejected_students.size() <= 1)
+    return rejected_students;
+  return {};
+}
+
 std::vector<Student> PlacementSystem::get_students() { return db_students; }
 
 void PlacementSystem::place_students() {
   for (auto &student : db_students) {
     auto student_school_choices = student.get_school_choices();
     bool placed = false;
-    for (auto &student_choice_registedSchool : student_school_choices) {
+
+    for (auto &student_chosen_registedSchool : student_school_choices) {
       if (student.get_computed_evaluation() <=
-          student_choice_registedSchool.get_cut_off()) {
-        auto placed_student = std::make_pair(student, student_school_choices);
-        // something happens here to add students placed into schools
+          student_chosen_registedSchool.get_cut_off()) {
+        placed_students.emplace_back(student, student_chosen_registedSchool);
+      } else {
+        rejected_students.emplace_back(student, student_chosen_registedSchool);
       }
     }
   }
+}
+
+void PlacementSystem::display_placement_results() {
+  // TODO display people who have been placed in schools and those who did not,
+  // separately!
 }
