90be21ae9930de4883703e56121ff25aa11bb3ba
blackprince001 <appiahboaduprince@gmail.com>
2023-03-22 14:05:12 +0000

    change character separating names from grades

--- assets/generate_students.py
+++ assets/generate_students.py
@@ -1,14 +1,14 @@
 from random import choice
 from defaults import student_names, grades, courses
 
-with open("./assets/students.txt", 'w') as file:
+with open("./students.txt", 'w') as file:
   grouped_students = []
   for name in student_names:
     student_courses = []
     for course in courses:
       student_courses.append(f"{course}:{choice(grades)}")
     
-    new_student = name + ", {" + ", ".join(student_courses) + "}" + "\n"
+    new_student = name + "- {" + ", ".join(student_courses) + "}" + "\n"
     grouped_students.append(new_student)
 
   for data in grouped_students:
