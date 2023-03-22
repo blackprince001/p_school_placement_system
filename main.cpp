293a038bb0abc7b5ccc584c3029cb87975cb4b1a
blackprince001 <appiahboaduprince@gmail.com>
2023-03-22 17:37:33 +0000

    move utility functions to separate sub-directory

--- main.cpp
+++ main.cpp
@@ -1,59 +1,26 @@
-#include <cstdio>
-#include <fstream>
-
 #include "PlacementSystem/placement_system.hpp"
 #include "SchoolSystem/SchoolSystem.hpp"
+#include "utils/utils.cpp"
 
 std::vector<Student> template_students;
 std::vector<RegistedSchool> template_schools;
 std::unordered_map<std::string, std::string> template_grades;
 
-std::vector<std::string> read_records_from_database(std::string &path) {
-  std::ifstream file(path);
-  std::vector<std::string> parsed_records;
-
-  if (!file.is_open()) {
-    return {};  // return an empty vector instead
-  }
-  std::string line;
-  while (std::getline(file, line)) {
-    parsed_records.push_back(line);
-  }
-  file.close();
-
-  return parsed_records;
-}
-
-std::vector<std::string> split_by(std::string &text, char ch) {
-  std::vector<std::string> texts;
-  std::string buf;
-  for (auto s : text) {
-    if (s == ch) {
-      texts.push_back(buf);
-      buf = "";
-      continue;
-    }
-    buf += s;
-  }
-  if (buf.length() > 0) texts.push_back(buf);
-  return texts;
-}
-
 int main() {
-  // TODO
-  // Make a global variable for registed courses for all candidates
-  // Make a template generated list of students preferrably in a text file to be
-  // read
-  std::string path_to_student_database = "../students.txt";
+    // TODO
+    // Make a global variable for registed courses for all candidates
+    // Make a template generated list of students preferrably in a text file to
+    // be read
+    std::string path_to_student_database = "../students.txt";
 
-  auto student_records = read_records_from_database(path_to_student_database);
+    auto student_records = read_records_from_database(path_to_student_database);
 
-  for (auto student_record : student_records) {
-    auto caught = split_by(
-        student_record,
-        '-');  // separate names from grades by splitting with character
-    for (auto item : caught) std::cout << item << "\n";
-  }
+    for (auto student_record : student_records) {
+        auto caught = split_by(
+            student_record,
+            '-');  // separate names from grades by splitting with character
+        for (auto item : caught) std::cout << item << "\n";
+    }
 
-  return 0;
+    return 0;
 }
---
+++ utils/utils.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+/**
+ * It reads a file and returns a vector of strings
+ * 
+ * @param path The path to the file to read from.
+ * 
+ * @return A vector of strings.
+ */
+std::vector<std::string> read_records_from_database(std::string &path) {
+    std::ifstream file(path);
+    std::vector<std::string> parsed_records;
+
+    if (!file.is_open()) {
+        return {};  // return an empty vector instead
+    }
+    std::string line;
+    while (std::getline(file, line)) {
+        parsed_records.push_back(line);
+    }
+    file.close();
+    return parsed_records;
+}
+
+/**
+ * It splits a string by a character
+ * 
+ * @param text The text to split
+ * @param ch The character to split by
+ * 
+ * @return A vector of strings
+ */
+std::vector<std::string> split_by(std::string &text, char ch) {
+    std::vector<std::string> texts;
+    std::string buf;
+    for (auto s : text) {
+        if (s == ch) {
+            texts.push_back(buf);
+            buf = "";
+            continue;
+        }
+        buf += s;
+    }
+    if (buf.length() > 0) texts.push_back(buf);
+    return texts;
+}
