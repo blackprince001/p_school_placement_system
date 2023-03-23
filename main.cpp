33f91374f9e8a5cca8786dab287a8451c2ada8f8
blackprince001 <appiahboaduprince@gmail.com>
2023-03-23 11:40:51 +0000

    first test of program

--- main.cpp
+++ main.cpp
@@ -1,27 +1,67 @@
+#include <unordered_map>
+
+#include "PlacementSystem/placement_system.cpp"
 #include "PlacementSystem/placement_system.hpp"
+#include "SchoolSystem/SchoolSystem.cpp"
 #include "SchoolSystem/SchoolSystem.hpp"
 #include "utils/utils.cpp"
 
 std::vector<Student> TEMPLATE_STUDENTS;
-std::vector<RegistedSchool> TEMPLATE_SCHOOLS;
-std::unordered_map<std::string, std::string> TEMPLATE_GRADES;
 
-int main() {
-    // TODO
-    // Make a global variable for registed courses for all candidates
-    // Make a template generated list of students preferrably in a text file to
-    // be read
+std::vector<RegistedSchool> TEMPLATE_SCHOOLS;
+
+std::vector<std::string> default_courses = {"Mathematics", "English Language",
+                                            "Science", "Social Science"};
+
+std::vector<std::string> &set_default_school_courses() {
+    return default_courses;
+}
+
+RegistedSchool KNUST("KNUST", set_default_school_courses(), 17);
+RegistedSchool Legon("UG-Legon", set_default_school_courses(), 15);
+RegistedSchool Uenr("UENR", set_default_school_courses(), 12);
+
+void preload_registedschools() {
+    for (auto item : {KNUST, Legon, Uenr}) TEMPLATE_SCHOOLS.push_back(item);
+}
+
+void preload_student_data() {
     std::string path_to_student_database = "../students.txt";
 
     auto student_records =
         util::read_records_from_database(path_to_student_database);
 
-    for (auto student_record : student_records) {
-        auto caught = util::split_by(
-            student_record,
-            '-');  // separate names from grades by splitting with character
-        for (auto item : caught) std::cout << item << "\n";
+    for (auto record : student_records) {
+        std::unordered_map<std::string, std::string> student_grades;
+
+        auto caught = util::split_by(record, '-');
+
+        std::string student_name = caught[0];
+        auto split_grades = util::split_by(caught[1], ',');
+
+        for (auto item : split_grades) {
+            auto course_and_grade = util::split_by(item, ':');
+            student_grades.emplace(course_and_grade[0], course_and_grade[1]);
+        }
+
+        Student nwStudent(student_name, student_grades, TEMPLATE_SCHOOLS);
+
+        TEMPLATE_STUDENTS.push_back(nwStudent);
     }
+}
+
+int main() {
+    preload_registedschools();
+
+    preload_student_data();
+
+    for (int n = 0; n < 3; ++n) TEMPLATE_STUDENTS[n].display_profile();
+
+    PlacementSystem WAEC(TEMPLATE_SCHOOLS, TEMPLATE_STUDENTS);
+
+    WAEC.place_students();
+
+    WAEC.display_placement_results("UENR");
 
     return 0;
 }
