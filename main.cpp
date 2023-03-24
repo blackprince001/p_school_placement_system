2d8f5e249db5f6e3958969361763f4616198a363
blackprince001 <appiahboaduprince@gmail.com>
2023-03-24 02:36:38 +0000

    document and refactor `main`

--- main.cpp
+++ main.cpp
@@ -1,52 +1,71 @@
-#include <unordered_map>
-
 #include "PlacementSystem/placement_system.cpp"
 #include "PlacementSystem/placement_system.hpp"
 #include "SchoolSystem/SchoolSystem.cpp"
 #include "SchoolSystem/SchoolSystem.hpp"
 #include "utils/utils.cpp"
 
-std::vector<Student> TEMPLATE_STUDENTS;
+/* Declaring two global vectors of type `Student` and `RegistedSchool`
+ * respectively. */
+std::vector<Student> template_students;
+std::vector<RegistedSchool> template_schools;
 
-std::vector<RegistedSchool> TEMPLATE_SCHOOLS;
-
+// change default course to default programmes
 std::vector<std::string> default_courses = {"Mathematics", "English Language",
                                             "Science", "Social Science"};
 
+/**
+ * It returns a reference to the vector of strings that is the default school
+ * courses
+ *
+ * @return A reference to the vector of strings.
+ */
 std::vector<std::string> &set_default_school_courses() {
     return default_courses;
 }
 
+/* Creating three objects of the class `RegistedSchool` and assigning them to
+the variables `KNUST`, `Legon` and `Uenr` respectively. */
 RegistedSchool KNUST("KNUST", set_default_school_courses(), 17);
 RegistedSchool Legon("UG-Legon", set_default_school_courses(), 15);
 RegistedSchool Uenr("UENR", set_default_school_courses(), 12);
 
+/**
+ * It takes a list of schools and pushes them into a vector
+ */
 void preload_registedschools() {
-    for (auto item : {KNUST, Legon, Uenr}) TEMPLATE_SCHOOLS.push_back(item);
+    for (auto item : {KNUST, Legon, Uenr}) template_schools.push_back(item);
 }
 
+/**
+ * It reads the student database,
+ * creates a student object for each student, and adds the student to the global
+ * vector of students
+ */
 void preload_student_data() {
-    std::string path_to_student_database = "../students.txt";
-
-    auto student_records =
-        util::read_records_from_database(path_to_student_database);
+    std::string fp = "../students.txt";
+    auto student_records = util::read_records_from_database(fp);
 
     for (auto record : student_records) {
         std::unordered_map<std::string, std::string> student_grades;
 
-        auto caught = util::split_by(record, '-');
+        // Split the name of the student from the grades obtained
+        auto split_name_from_grades = util::split_by(record, '-');
 
-        std::string student_name = caught[0];
-        auto split_grades = util::split_by(caught[1], ',');
+        // Assign the student name and grades to string and pair objects
+        std::string student_name = split_name_from_grades[0];
+        auto split_grades = util::split_by(split_name_from_grades[1], ',');
 
+        // Iterate over the pair iterable `split_grades`, append them to
+        // student_grades
         for (auto item : split_grades) {
             auto course_and_grade = util::split_by(item, ':');
             student_grades.emplace(course_and_grade[0], course_and_grade[1]);
         }
 
-        Student nwStudent(student_name, student_grades, TEMPLATE_SCHOOLS);
-
-        TEMPLATE_STUDENTS.push_back(nwStudent);
+        // create student objects and add them to a collection of present
+        // students.
+        Student nwStudent(student_name, student_grades, template_schools);
+        template_students.push_back(nwStudent);
     }
 }
 
@@ -55,12 +74,15 @@ int main() {
 
     preload_student_data();
 
-    for (int n = 0; n < 3; ++n) TEMPLATE_STUDENTS[n].display_profile();
+    for (int n = 0; n < 3; ++n) template_students[n].display_profile();
 
-    PlacementSystem WAEC(TEMPLATE_SCHOOLS, TEMPLATE_STUDENTS);
-
+    // Create a PlacementSystem object called WAEC
+    PlacementSystem WAEC(template_schools, template_students);
+    // Place students using system member functions
     WAEC.place_students();
-
+    // Display the student who have been placed in a particular University by
+    // name Default University can be inferenced from the global variables at
+    // the top of this source. {KNUST, UG-Legon, UENR}.
     WAEC.display_placement_results("UENR");
 
     return 0;
