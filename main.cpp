7059bdd0b3e779fc1e23690ae0d2d7d4319cde31
blackprince001 <appiahboaduprince@gmail.com>
2023-03-25 13:56:44 +0000

    second version of program resting
    create programme objects instead of courses
    
    add programmes to school objects
    
    add base programmes to all applicant student objects

--- main.cpp
+++ main.cpp
@@ -9,9 +9,13 @@
 std::vector<Student> template_students;
 std::vector<RegistedSchool> template_schools;
 
-// change default course to default programmes
-std::vector<std::string> default_courses = {"Mathematics", "English Language",
-                                            "Science", "Social Science"};
+/* A global variable that is used to store the default courses that
+are offered by the schools. */
+std::vector<Programme> default_programmes;
+
+std::vector<std::string> default_programme_names = {
+    "Computer Engineering", "Electrical Engineering", "Computer Science",
+    "Mechanical Engineering"};
 
 /**
  * It returns a reference to the vector of strings that is the default school
@@ -19,15 +23,19 @@ std::vector<std::string> default_courses = {"Mathematics", "English Language",
  *
  * @return A reference to the vector of strings.
  */
-std::vector<std::string> &set_default_school_courses() {
-    return default_courses;
+std::vector<Programme> &set_default_school_programmes() {
+    for (auto programme_name : default_programme_names) {
+        Programme nwProgramme(programme_name, 15);
+        default_programmes.push_back(nwProgramme);
+    }
+    return default_programmes;
 }
 
 /* Creating three objects of the class `RegistedSchool` and assigning them to
 the variables `KNUST`, `Legon` and `Uenr` respectively. */
-RegistedSchool KNUST("KNUST", set_default_school_courses(), 17);
-RegistedSchool Legon("UG-Legon", set_default_school_courses(), 15);
-RegistedSchool Uenr("UENR", set_default_school_courses(), 12);
+RegistedSchool KNUST("KNUST", set_default_school_programmes());
+RegistedSchool Legon("UG-Legon", set_default_school_programmes());
+RegistedSchool Uenr("UENR", set_default_school_programmes());
 
 /**
  * It takes a list of schools and pushes them into a vector
@@ -61,10 +69,10 @@ void preload_student_data() {
             auto course_and_grade = util::split_by(item, ':');
             student_grades.emplace(course_and_grade[0], course_and_grade[1]);
         }
-
         // create student objects and add them to a collection of present
         // students.
-        Student nwStudent(student_name, student_grades, template_schools);
+        Student nwStudent(student_name, student_grades, template_schools,
+                          {"Computer Engineering", "Computer Science"});
         template_students.push_back(nwStudent);
     }
 }
@@ -83,7 +91,7 @@ int main() {
     // Display the student who have been placed in a particular University by
     // name Default University can be inferenced from the global variables at
     // the top of this source. {KNUST, UG-Legon, UENR}.
-    WAEC.display_placement_results("UENR");
+    WAEC.display_placement_results("KNUST");
 
     return 0;
 }
